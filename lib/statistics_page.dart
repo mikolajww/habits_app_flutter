3133bad56010c3e5fe04bb506ae1f9251ed70378
mikolajww <mikolaj.w.w17@gmail.com>
2021-02-17 17:22:00 +0100

    Changed chart provider

--- lib/statistics_page.dart
+++ lib/statistics_page.dart
@@ -1,6 +1,6 @@
 import 'package:Habitect/data/to_do_item.dart';
 import 'package:Habitect/services/to_do_service.dart';
-import 'package:bezier_chart/bezier_chart.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -55,59 +55,59 @@ class _StatisticsPageState extends State<StatisticsPage> with SingleTickerProvid
   }
 }
 
-getWeeklyStatistics(fromDate, toDate, context) {
+charts.Series<TimeSeriesTodo, DateTime> getWeeklyStatistics(DateTime fromDate, DateTime toDate, BuildContext context) {
   final ToDoService toDoService = Provider.of(context);
 
-  final weeklyTodos = toDoService.todos.where((element) => element.date.isBefore(toDate) && element.date.isAfter(fromDate)).toList();
+  final weeklyTodos = toDoService.todos
+      .where((element) => element.date.isBefore(toDate) && element.date.isAfter(fromDate))
+      .toList()
+        ..sort((ToDoItem a, ToDoItem b) => a.date.compareTo(b.date));
+
+  final daysToGenerate = toDate.difference(fromDate).inDays + 1;
+  var days = List.generate(daysToGenerate, (i) => DateTime(fromDate.year, fromDate.month, fromDate.day + (i)));
 
   Map<DateTime, List<ToDoItem>> weeklyTodosMapped = Map();
+
+  days.forEach((element) {
+    var key = DateTime(element.year, element.month, element.day);
+    weeklyTodosMapped.putIfAbsent(key, () => null);
+    weeklyTodosMapped[key] = [];
+  });
   weeklyTodos.forEach((element) {
     var key = DateTime(element.date.year, element.date.month, element.date.day);
-    if (!weeklyTodosMapped.containsKey(key)) {
-      weeklyTodosMapped.putIfAbsent(key, () => null);
-      weeklyTodosMapped[key] = [];
-    }
     weeklyTodosMapped[key].add(element);
   });
 
-  List<DataPoint> weeklyData = [];
-
+  List<TimeSeriesTodo> weeklyData = [];
   for (var entry in weeklyTodosMapped.entries) {
     var completion = 100 * entry.value.where((ToDoItem element) => element.isCompleted).length / entry.value.length;
-    weeklyData.add(DataPoint<DateTime>(value: completion, xAxis: entry.key));
+    if (completion.isNaN) completion = 0;
+    weeklyData.add(TimeSeriesTodo(entry.key, completion));
   }
-  return weeklyData;
+  return charts.Series<TimeSeriesTodo, DateTime>(
+      id: 'Todo',
+      colorFn: (datum, index) => charts.MaterialPalette.green.shadeDefault,
+      domainFn: (datum, index) => datum.date,
+      measureFn: (datum, index) => datum.percentComplete,
+      data: weeklyData);
 }
 
 Widget WeeklyStatistics(BuildContext context) {
-  final fromDate = DateTime.now().subtract(Duration(days: 7));
+  final fromDate = DateTime.now().subtract(Duration(days: 6));
   final toDate = DateTime.now();
-
+  final charts.Series<TimeSeriesTodo, DateTime> series = getWeeklyStatistics(fromDate, toDate, context);
   return Center(
-    child: Container(
-      color: Colors.red,
-      height: MediaQuery.of(context).size.height / 2,
-      width: MediaQuery.of(context).size.width,
-      child: BezierChart(
-        fromDate: fromDate,
-        bezierChartScale: BezierChartScale.WEEKLY,
-        toDate: toDate,
-        selectedDate: toDate,
-        series: [
-          BezierLine(
-            label: "% completed",
-            data: [...getWeeklyStatistics(fromDate, toDate, context)],
-          ),
-        ],
-        config: BezierChartConfig(
-          verticalIndicatorStrokeWidth: 3.0,
-          verticalIndicatorColor: Colors.black26,
-          showVerticalIndicator: true,
-          verticalIndicatorFixedPosition: false,
-          backgroundColor: Color(0xFF2e2e2e),
-          footerHeight: 30.0,
-        ),
-      ),
+    child: charts.TimeSeriesChart(
+      [series],
+      animate: true,
+      dateTimeFactory: const charts.LocalDateTimeFactory(),
     ),
   );
+}
+
+class TimeSeriesTodo {
+  final DateTime date;
+  final double percentComplete;
+
+  TimeSeriesTodo(this.date, this.percentComplete);
 }
